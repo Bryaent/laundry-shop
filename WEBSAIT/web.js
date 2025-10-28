4b07994dbe73515c62abb857fe2e226396cabd7a
Bryant <bryantrondero@gmail.com>
2025-10-28 12:46:12 +0800

    updated

--- WEBSAIT/web.js
+++ WEBSAIT/web.js
@@ -1,6 +1,6 @@
 function showPage(pageId) {
   // Alisin ang 'active' sa lahat ng sections
-  document.querySelectorAll('kontainer').forEach(sec => sec.classList.remove('active'));
+  document.querySelectorAll('.kontainer').forEach(sec => sec.classList.remove('active'));
   
   // Idagdag ang 'active' class sa piniling section
   document.getElementById(pageId).classList.add('active');
