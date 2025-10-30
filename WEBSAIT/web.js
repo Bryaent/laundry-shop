1351eb5426752dfdc269ac78ed19b8a9f2e5f4c8
Bryant <bryantrondero@gmail.com>
2025-10-31 01:38:44 +0800

    bagong website

--- WEBSAIT/web.js
+++ WEBSAIT/web.js
@@ -1,7 +1,17 @@
-function showPage(pageId) {
-  // Alisin ang 'active' sa lahat ng sections
-  document.querySelectorAll('.kontainer').forEach(sec => sec.classList.remove('active'));
-  
-  // Idagdag ang 'active' class sa piniling section
-  document.getElementById(pageId).classList.add('active');
-}
+ function showService(service) {
+      // Hide both sections initially
+      document.getElementById('fullService').style.display = 'none';
+      document.getElementById('selfSelect').style.display = 'none';
+      
+      // Show the selected section
+      document.getElementById(service).style.display = 'block';
+    }
+
+    function toggleOption(option) {
+      const button = document.getElementById(option);
+      if (button.classList.contains('active')) {
+        button.classList.remove('active');
+      } else {
+        button.classList.add('active');
+      }
+    }
